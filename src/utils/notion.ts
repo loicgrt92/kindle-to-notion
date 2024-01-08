97585bf51f5e87764078baafeb712d8ea63113d3
loicgrt92 <88844760+loicgrt92@users.noreply.github.com>
2024-01-08 18:11:31 +0100

    Clean

--- src/utils/notion.ts
+++ src/utils/notion.ts
@@ -35,7 +35,7 @@ export const makeHighlightsBlocks = (
   type: BlockType
 ): Block[] => {
   return [
-    ...makeBlocks([" 🎀 Highlights"], BlockType.heading_1),
+    ...makeBlocks([" 🗒️ Highlights"], BlockType.heading_1),
     ...makeBlocks(highlights, type),
   ];
 };
